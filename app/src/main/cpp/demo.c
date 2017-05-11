fbb7e618a9f532a66aa54ec040028562ff62f463
huhuang03 <huhuang03@gmail.com>
2017-05-11 11:30:27 +0800

    auto commit

--- app/src/main/cpp/demo.c
+++ app/src/main/cpp/demo.c
@@ -11,3 +11,27 @@ Java_common_com_libsubstrate_MainActivity_stringFromJNI(JNIEnv *env, jobject ins
 
     return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI ");
 }
+
+MSConfig(MSFilterExecutable, "/system/bin/app_process")
+
+// this is a macro that uses __attribute__((__constructor__))
+MSInitialize {
+    // ... code to run when extension is loaded
+}
+
+
+static jint (*_Resources$getColor)(JNIEnv *jni, jobject _this, ...);
+
+static jint $Resources$getColor(JNIEnv *jni, jobject _this, jint rid) {
+    jint color = _Resources$getColor(jni, _this, rid);
+    return color & ~0x0000ff00 | 0x00ff0000;
+}
+
+static void OnResources(JNIEnv *jni, jclass resources, void *data) {
+    jmethodID method = (*jni)->GetMethodID(jni, resources, "getColor", "(I)I");
+    if (method != NULL)
+        MSJavaHookMethod(jni, resources, method,
+                         &$Resources$getColor, &_Resources$getColor);
+}
+
+//MSJavaHookClassLoad(NULL, "android/content/res/Resources", &OnResources);
